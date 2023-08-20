5e87fba8522522b3f00413f8f7c5b0ae067d6193
PalmTree2198 <PalmTree2198>
2023-08-21 05:08:52 +0530

    Made some changes in the code

--- Drone_Detection.py
+++ Drone_Detection.py
@@ -21,66 +21,64 @@ class AudioUtil():
     def rechannel(aud, new_channel):
         sig, sr = aud
 
-    if (sig.shape[0] == new_channel):
-        return aud
+        if sig.shape[0] == new_channel:
+            return aud
 
-    if (new_channel == 1):
-        resig = sig[:1, :]
-    else:
-        resig = torch.cat([sig, sig])
+        if new_channel == 1:
+            resig = sig[:1, :]
+        else:
+            resig = torch.cat([sig, sig])
 
-    return ((resig, sr))
+        return (resig, sr)
 
     @staticmethod
     def resample(aud, newsr):
         sig, sr = aud
 
-    if (sr == newsr):
-        return aud
+        if sr == newsr:
+            return aud
 
-    num_channels = sig.shape[0]
-    resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
-    if (num_channels > 1):
-        retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
+        num_channels = sig.shape[0]
+        resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
+        if num_channels > 1:
+            retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
             resig = torch.cat([resig, retwo])
-        return ((resig, newsr))
+        return (resig, newsr)
 
     @staticmethod
     def pad_trunc(aud, max_ms):
         sig, sr = aud
         num_rows, sig_len = sig.shape
-        max_len = sr//1000 * max_ms
+        max_len = sr // 1000 * max_ms
 
-        if (sig_len > max_len):
-            sig = sig[:,:max_len]
-
-        elif (sig_len < max_len):
+        if sig_len > max_len:
+            sig = sig[:, :max_len]
+        elif sig_len < max_len:
             pad_begin_len = random.randint(0, max_len - sig_len)
             pad_end_len = max_len - sig_len - pad_begin_len
             pad_begin = torch.zeros((num_rows, pad_begin_len))
             pad_end = torch.zeros((num_rows, pad_end_len))
-
             sig = torch.cat((pad_begin, sig, pad_end), 1)
 
         return (sig, sr)
 
     @staticmethod
     def time_shift(aud, shift_limit):
-        sig,sr = aud
+        sig, sr = aud
         _, sig_len = sig.shape
         shift_amt = int(random.random() * shift_limit * sig_len)
         return (sig.roll(shift_amt), sr)
 
     @staticmethod
     def spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None):
-        sig,sr = aud
+        sig, sr = aud
         top_db = 80
-        spec = transforms.MelSpectrogram(sr, n_fft = n_fft, hop_length=hop_len, n_mels = n_mels)(sig)
+        spec = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(sig)
         spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
-        return (spec)
+        return spec
 
     @staticmethod
-    def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks = 1):
+    def spectro_augment(spec, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1):
         _, n_mels, n_steps = spec.shape
         mask_value = spec.mean()
         aug_spec = spec
@@ -94,6 +92,7 @@ class AudioUtil():
             aug_spec = transforms.TimeMasking(time_mask_param)(aug_spec, mask_value)
 
         return aug_spec
+
 
 class DroneDS(Dataset):
     def __init__(self, df):
